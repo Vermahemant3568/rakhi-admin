a5c549dc9094afa01b4fb57f5381d7c84d59d464
Hemant Verma <vermahemant3568@gmail.com>
2026-04-14 12:36:52 +0530

    baseurl

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/admin',
+  baseURL: 'http://13.234.114.78/api/admin',
   headers: { 'Content-Type': 'application/json' },
 });
 
