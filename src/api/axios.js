ade39df66db49e3a7bba50426ea168caa2987f9a
Hemant Verma <vermahemant3568@gmail.com>
2026-04-14 15:57:56 +0530

    otp2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.rakhihealth.in/api/admin',
+  baseURL: (import.meta.env.VITE_API_URL ?? 'https://api.rakhihealth.in/api') + '/admin',
   headers: { 'Content-Type': 'application/json' },
 });
 
