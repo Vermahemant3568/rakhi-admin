5ba189c541183b88830b5b82141eb6e4b458f705
Hemant Verma <vermahemant3568@gmail.com>
2026-04-16 20:20:31 +0530

    fix: switch back to domain https://api.rakhihealth.in

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL ?? 'http://3.6.161.169/api') + '/admin', // baseURL: (import.meta.env.VITE_API_URL ?? 'https://api.rakhihealth.in/api') + '/admin',
+  baseURL: (import.meta.env.VITE_API_URL ?? 'https://api.rakhihealth.in/api') + '/admin',
   headers: { 'Content-Type': 'application/json' },
 });
 
